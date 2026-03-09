53bd8f6cf7b9eecef0aaf6f2a512cf10cad44931
Jhonata Jansen <codejhonatajansen@gmail.com>
2026-03-09 10:23:05 -0300

    commit inicial

---
+++ src/main/java/com/devjansen/crud/usuario/CrudUsuario.java
@@ -0,0 +1,13 @@
+package com.devjansen.crud.usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUsuario {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUsuario.class, args);
+	}
+
+}
